335f442860252a7d2db1da043d7319ed1cc97360
Suraj Kanyal <59767099+surajkanyal@users.noreply.github.com>
2023-08-10 23:50:43 +0530

    Delete Band_name_generator.py

--- Band_name_generator.py
+++
@@ -1,7 +0,0 @@
-def name_generator(name,petname):
-    return name+" "+petname
-if __name__ =="__main__":
-    a = input("Enter Your Firstname: ")
-    b = input("Your First pet name : ")
-    c = name_generator(a,b)
-    print(f"Your new name is : {c}")
